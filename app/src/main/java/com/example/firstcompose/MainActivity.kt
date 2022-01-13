3da66834dbeeb1a87d6408c199bc8f3067d7b248
Yash <yashrajsingh807@gmail.com>
2022-01-13 21:53:51 +0530

    Learned about Box and made an Image Card...

--- app/src/main/java/com/example/firstcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -4,21 +4,31 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.firstcompose.ui.theme.FirstComposeTheme
 import com.example.firstcompose.ui.theme.Shapes
 
@@ -26,36 +36,52 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val painter = painterResource(id = R.drawable.wallpaper)
+            val description = "It is a Wallpaper Image."
+            val title = "Nice looking Wallpaper"
+            Box(modifier = Modifier.fillMaxWidth(0.5f)) {
+                ImageCard(painter = painter, contentDescription = description, title = title)
+            }
+        }
+    }
+}
 
-            //MODIFIERS - Always work Sequentially
-
-            Column(
-                modifier = Modifier
-                    .background(Color.Cyan)
-                    .fillMaxHeight(0.5f)
-                    .width(600.dp)
-                    //.requiredWidth(600.dp)
-                    .border(5.dp, color = Color.LightGray)
-                    .padding(5.dp)
-                    .border(5.dp, color = Color.Black)
-                    .padding(5.dp)
-                    .border(5.dp, color = Color.White)
-                    .padding(10.dp)
-            ) {
-                Text(text = "Yash", modifier = Modifier.offset(50.dp))
-                Spacer(modifier = Modifier.height(20.dp))
-                Text(text = "Raj")
-                Text(text = "Hi", modifier = Modifier.clickable {
-                    Toast.makeText(this@MainActivity,"Button Clicked!", Toast.LENGTH_SHORT).show()
-                })
-                Text(text = "There",
-                    modifier = Modifier.border(5.dp, color = Color.Red)
-                        .padding(5.dp)
-                        .border(5.dp, color = Color.Gray)
-                        .offset(10.dp, 10.dp)
-                        .border(5.dp, color = Color.Yellow)
-                        .padding(10.dp)
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription : String,
+    title : String,
+    modifier: Modifier = Modifier
+){
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(10),
+        elevation = 5.dp
+    ) {
+        Box(modifier = Modifier.height(200.dp)){
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = 300f
+                    )
                 )
+            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp),
+                contentAlignment = Alignment.BottomStart
+            ){
+                Text(title, style = TextStyle(color = Color.White, fontSize = 12.sp, fontFamily = FontFamily.Monospace))
             }
         }
     }

85bdc9d87b440c989ccdcd9d3e808e3675e97146
Yash Raj Singh <yashrajsingh807@gmail.com>
2022-02-02 22:04:05 +0530

    Made a Volume Knob UI

--- app/src/main/java/com/example/firstcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -1,57 +1,67 @@
 package com.example.firstcompose
 
 import android.os.Bundle
-import android.widget.Toast
+import android.view.MotionEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.*
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.layout.boundsInWindow
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ChainStyle
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.ConstraintSet
-import androidx.constraintlayout.compose.Dimension
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlin.math.PI
+import kotlin.math.atan2
+import kotlin.math.roundToInt
 
 
+@ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
                 Box(
                     contentAlignment = Alignment.Center,
-                    modifier = Modifier.fillMaxSize()
-                ){
-                    Column(modifier = Modifier.fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.SpaceAround
-                        ) {
-                        circularProgressBar(percent = 0.8f, number = 500, animDelay = 0)
-                        circularProgressBar(percent = 0.3f, number = 1000, animDelay = 1000)
-                        circularProgressBar(percent = 0.65f, number = 2000, animDelay = 2000)
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .border(1.dp, Color.Green, RoundedCornerShape(10.dp))
+                            .padding(30.dp)
+                    ) {
+                        var volume by remember {
+                            mutableStateOf(0f)
+                        }
+
+                        val barCount = 20
+                        VolumeKnob(
+                            modifier = Modifier.size(100.dp)
+                        ){
+                            volume = it
+                        }
+                        Spacer(modifier = Modifier.width(20.dp))
+                        VolumeBar(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(30.dp),
+                            activeBars = (volume * barCount).roundToInt(),
+                            barCount = barCount
+                        )
                     }
                 }
         }
@@ -59,54 +69,90 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun circularProgressBar(
-    percent:Float,
-    number:Int,
-    fontSize : TextUnit = 28.sp,
-    radius : Dp = 50.dp,
-    color : Color = Color(0xff457b9d),
-    strokeWidth : Dp = 10.dp,
-    animDuration:Int = 1000,
-    animDelay:Int
-){
-    var animationPlayed by remember {
-        mutableStateOf(false)
-    }
-
-    val curPercentage = animateFloatAsState(
-        targetValue = if (animationPlayed) percent else 0f,
-        animationSpec = tween(
-            durationMillis = animDuration,
-            delayMillis = animDelay
-        )
-    )
-
-    LaunchedEffect(key1 = true){
-        animationPlayed = true
-    }
-
-
-    Box(
+fun VolumeBar(
+    modifier: Modifier = Modifier,
+    activeBars : Int = 0,
+    barCount:Int = 10
+) {
+    BoxWithConstraints(
         contentAlignment = Alignment.Center,
-        modifier = Modifier.size(radius*2f)
-    ){
-        Canvas(modifier = Modifier.size(radius*2f)){
-            drawArc(
-                color = color,
-                -90f,
-                360 * curPercentage.value,
-                useCenter = false,
-                style = Stroke(strokeWidth.toPx(),
-                    cap = StrokeCap.Round
-                    )
-            )
+        modifier = modifier
+    ) {
+        val barWidth = remember {
+            constraints.maxWidth / (2f * barCount)
         }
-        
-        Text(text = (curPercentage.value * number).toInt().toString(),
-            color = Color(0xff1d3557),
-            fontSize = fontSize,
-            fontWeight = FontWeight.Bold
-            )
-        
+        Canvas(modifier = modifier){
+            for (i in 0 until barCount)
+            {
+                drawRoundRect(
+                    color = if(i in 0..activeBars) Color.Green else Color.DarkGray,
+                    topLeft = Offset(i * barWidth * 2f + barWidth / 2f, 0f),
+                    size = Size(barWidth, constraints.maxHeight.toFloat()),
+                    cornerRadius = CornerRadius(2f)
+                )
+            }
+        }
     }
+}
+
+@ExperimentalComposeUiApi
+@Composable
+fun VolumeKnob(
+    modifier: Modifier = Modifier,
+    limitingAngle: Float = 25f,
+    onValueChange : (Float) -> Unit
+){
+    var rotation by remember{
+        mutableStateOf(limitingAngle)
+    }
+
+    var touchX by remember {
+        mutableStateOf(0f)
+    }
+    var touchY by remember {
+        mutableStateOf(0f)
+    }
+    var centerX by remember {
+        mutableStateOf(0f)
+    }
+    var centerY by remember {
+        mutableStateOf(0f)
+    }
+
+    Image(
+        painter = painterResource(id = R.drawable.volume_knob),
+        contentDescription = "Music knob",
+        modifier = modifier
+            .fillMaxSize()
+            .onGloballyPositioned {
+                val windowBounds = it.boundsInWindow()
+                centerX = windowBounds.size.width / 2f
+                centerY = windowBounds.size.height / 2f
+            }
+            .pointerInteropFilter { event ->
+                touchX = event.x
+                touchY = event.y
+                val angle = -atan2(centerX - touchX, centerY - touchY) * (180f / PI).toFloat()
+
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN,
+                    MotionEvent.ACTION_MOVE -> {
+                        if (angle !in -limitingAngle..limitingAngle) {
+                            val fixedAngle = if (angle in -180f..-limitingAngle) {
+                                360f + angle
+                            } else {
+                                angle
+                            }
+                            rotation = fixedAngle
+
+                            val percent = (fixedAngle - limitingAngle) / (360f - 2 * limitingAngle)
+                            onValueChange(percent)
+                            true
+                        } else false
+                    }
+                    else -> false
+                }
+            }
+            .rotate(rotation)
+    )
 }
